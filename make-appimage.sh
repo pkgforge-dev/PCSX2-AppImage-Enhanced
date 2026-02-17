f2088a19ae073e121be5200f8b9d6f2c9187659c
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-02-17 16:50:10 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -4,7 +4,7 @@ set -eu
 
 ARCH=$(uname -m)
 VERSION=$(pacman -Q pcsx2-git | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export x86_64 VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
